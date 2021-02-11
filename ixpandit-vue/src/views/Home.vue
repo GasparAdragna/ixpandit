c97f52c290fa44198bea6af7b0793fc448a8acf5
Gaspar <gaspar.jac@hotmail.com>
2021-02-11 20:28:36 -0300

    Connected with API List of pokemons showing

--- ixpandit-vue/src/views/Home.vue
+++ ixpandit-vue/src/views/Home.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="home">
-    <v-row>
-      <v-col> </v-col>
+    <v-row v-if="!ready">
+      <v-col cols="12" justify="center" class="text-center">
+        <v-progress-circular
+          :size="50"
+          color="primary"
+          indeterminate
+          class="text-center"
+        ></v-progress-circular>
+        <h2>Loading pokemons...</h2>
+      </v-col>
+    </v-row>
+    <v-row v-else>
+      <v-col
+        cols="12"
+        v-for="(pokemon, index) in pokemons.results"
+        :key="index"
+      >
+        {{ pokemon.name }}
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -9,21 +26,33 @@
 <script>
 var Pokedex = require("pokedex-promise-v2");
 var P = new Pokedex();
+const Swal = require("sweetalert2");
 export default {
   name: "Home",
   data() {
     return {
-      pokemons: {}
+      pokemons: {},
+      ready: false
     };
   },
   created: async function() {
-    P.getPokemonByName("eevee") // with Promise
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log("There was an ERROR: ", error);
-      });
+    this.ready = false;
+    if (localStorage.pokemons) {
+      P.getPokemonsList() // with Promise
+        .then(function(response) {
+          localStorage.setItem("pokemons", JSON.stringify(response));
+        })
+        .catch(function() {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong while trying to fetch the pokemons!"
+          });
+        });
+    }
+    let retrievedObject = localStorage.getItem("pokemons");
+    this.pokemons = JSON.parse(retrievedObject);
+    this.ready = true;
   }
 };
 </script>
