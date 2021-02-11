bf8d8ce03eb163d225a0840473c8de2aad725a4f
Gaspar <gaspar.jac@hotmail.com>
2021-02-11 20:57:03 -0300

    Added Pokemon Component and "pagination"

---
+++ ixpandit-vue/src/components/Pokemon.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card class="mx-auto">
+    <v-card-text>
+      <p class="display-1 text--primary mb-0">
+        {{ capitalizedName }}
+      </p>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn text color="deep-purple accent-4">
+        Learn More
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Pokemon",
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    capitalizedName() {
+      return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped></style>
--- ixpandit-vue/src/views/Home.vue
+++ ixpandit-vue/src/views/Home.vue
@@ -14,15 +14,23 @@
     <v-row v-else>
       <v-col cols="12" class="mt-5">
         <h1>Search your favourite Pokemon...</h1>
-        <v-text-field v-model="searchInput">Search por pokemon...</v-text-field>
+        <v-text-field v-model="searchInput" placeholder="Enter Pokemon Name..."
+          >Search por pokemon...</v-text-field
+        >
       </v-col>
-      <v-col
-        cols="12"
-        v-for="(pokemon, index) in filteredPokemons"
-        :key="index"
-      >
-        {{ pokemon.name }}
-      </v-col>
+      <v-row>
+        <v-col
+          cols="12"
+          lg="3"
+          v-for="(pokemon, index) in filteredPokemons"
+          :key="index"
+        >
+          <Pokemon :name="pokemon.name"></Pokemon>
+        </v-col>
+      </v-row>
+      <v-btn class="mt-5" color="primary" @click="morePokemons()" block>
+        Load more Pokemons
+      </v-btn>
     </v-row>
   </div>
 </template>
@@ -31,13 +39,16 @@
 var Pokedex = require("pokedex-promise-v2");
 var P = new Pokedex();
 const Swal = require("sweetalert2");
+import Pokemon from "../components/Pokemon.vue";
 export default {
   name: "Home",
+  components: { Pokemon },
   data() {
     return {
       pokemons: {},
       searchInput: null,
-      ready: false
+      ready: false,
+      limit: 10
     };
   },
   created: async function() {
@@ -68,7 +79,12 @@ export default {
             item.name.toUpperCase().indexOf(this.searchInput.toUpperCase()) > -1
         );
       }
-      return result;
+      return result.slice(0, this.limit);
+    }
+  },
+  methods: {
+    morePokemons() {
+      this.limit += 10;
     }
   }
 };
